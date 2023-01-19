8cd121fd74caf4eeeebbfdaad83a39755c85d5ab
Wayne Zeng <wayne@gimi65536.xyz>
2023-01-19 15:52:54 +0900

    Add basic information for the plugin

---
+++ wp-content/plugins/gimi65536-tools/gimi65536-tools.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       	 gimi65536's Tools
+ * Plugin URI:           https://blog.gimi65536.xyz/
+ * Description:       	 A custom plugin used for gimi65536'x wordpress blog
+ * Version:           	 0.0.1
+ * Requires at least:    6.0.0
+ * Requires PHP:         7.4
+ * Author:            	 Wayne Zeng
+ * Author URI:        	 https://blog.gimi65536.xyz/
+ * Text Domain:       	 gimi65536-tools
+ * Domain Path:          /language
+ */
